fa1d2ccfe224aa67eb6b49d02f663b489b7a83a9
ridwaanhall <erbyl14@gmail.com>
2024-12-09 13:33:14 +0700

    add index route to handle POST requests and respond to Telegram messages

--- api/index.py
+++ api/index.py
@@ -1,11 +1,21 @@
-from flask import Flask
+from flask import Flask, Response, request, jsonify
+from Controller.StudentController import Answer, Message
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        try:
+            msg = request.get_json()
+            if not msg:
+                return jsonify({'error': 'Invalid input'}), 400
 
-@app.route('/about')
-def about():
-    return 'About'
+            chat_id, incoming_question = Message.message_parser(msg)
+            answer = Answer.generate_answer(incoming_question)
+            Message.send_message_telegram(chat_id, answer)
+            return Response('ok', status=200)
+        except Exception as e:
+            return jsonify({'error': str(e)}), 500
+    else:
+        return "<h1>You're lost</h1>"
